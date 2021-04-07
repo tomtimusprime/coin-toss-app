e021411645780218f1e5999005231a1f5dfc4251
Tom Black <tom.black06007@gmail.com>
2021-04-06 20:16:40 -0700

    Added the scaffolding to this project.

---
+++ src/Coin.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Coin extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            heads: false
+        }
+    }
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Coin;
